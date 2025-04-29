947a9f030eb2341ab40e4c5a290c1c896a96aa4f
shokhazavr1104 <sahruzasurov8@gmail.com>
2025-04-29 19:13:17 +0500

    pochti konets first section

---
+++ temps/secondPage/secondPage.js
@@ -0,0 +1,22 @@
+import { Swiper } from './swiper.min.js';
+
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    effect: "fade",
+    loop: true,
+    autoplay: {
+        delay: 5000,
+        disableOnInteraction: false,
+    },
+    // navigation: {
+    //     nextEl: ".swiper-button-next",
+    //     prevEl: ".swiper-button-prev",
+    // },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+        renderBullet: function (index, className) {
+            return '<span class="' + className + '">' + (index + 1) + "</span>";
+        },
+    },
+});
