50b232a4bf7436271c92bbe7350dea93244595f4
Amir <amirjonabduxoliqov222@gmail.com>
2025-05-05 14:52:51 +0500

    .

---
+++ temps/temp-two/swiper.js
@@ -0,0 +1,18 @@
+// var swiper = new Swiper(".mySwiper", {
+//     slidesPerView: 5,
+//     spaceBetween: 30,
+//     loop: true,
+//     pagination: {
+//       el: ".swiper-pagination",
+//       clickable: true,
+//     },
+//   });
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 5,
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    }
+});
