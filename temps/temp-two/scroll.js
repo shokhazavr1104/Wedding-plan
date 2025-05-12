7cd83c76eb3f8018e7716eaafef5913f873efbcd
Amir <amirjonabduxoliqov222@gmail.com>
2025-05-12 18:35:03 +0500

    add animations

---
+++ temps/temp-two/scroll.js
@@ -0,0 +1,41 @@
+
+
+// function fnc() {
+//     if (window.screenY >= 500) {
+//         sectionSwiper.classlist.add('animate__fadeInUp')
+//     }
+// }
+
+
+
+const sectionSwiper = document.querySelector('.swiper-galery-cont');
+const guesttitlesection = document.querySelector('.top-guest-title-box');
+const guestgalerysec = document.querySelector('.top-guest-galery-box');
+const formcont = document.querySelector('.form-container');
+const newstitle = document.querySelector('.news-title-box');
+const newsbox = document.querySelector('.news-galery-box');
+
+
+window.addEventListener('scroll', (() => {
+    console.log(window.scrollY);
+    if (window.scrollY >= 1260) {
+        sectionSwiper.classList.add('animate__fadeInUp');
+    }
+    if (window.scrollY >= 1750) {
+        guesttitlesection.classList.add('animate__fadeInRight');
+    }
+
+    if (window.scrollY >= 1900) {
+        guestgalerysec.classList.add('animate__fadeInUp');
+    }
+    if (window.scrollY >= 2250) {
+        formcont.classList.add('animate__fadeInLeft');
+    }
+    if (window.scrollY >= 3572) {
+        newstitle.classList.add('animate__fadeInRight');
+    }
+
+    if (window.scrollY >= 3650) {
+        newsbox.classList.add('animate__fadeInUp');
+    }
+}))
