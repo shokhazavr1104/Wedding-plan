cef465782a512b8ce45a3132b9ef9c9391608334
shokhazavr1104 <sahruzasurov8@gmail.com>
2025-06-03 19:30:36 +0500

    a

--- temps/secondPage/secondPage.js
+++ temps/secondPage/secondPage.js
@@ -128,3 +128,156 @@ const x = setInterval(function () {
         document.getElementById("seconds").textContent = "00";
     }
 }, 1000);
+
+
+
+
+
+
+
+const section = document.querySelector('.section-story');
+
+const titleH2 = document.querySelector(".story-h2 ");
+const titleP = document.querySelector(".story-p");
+const titleHr = document.querySelector(".story-hr-block ");
+
+const titleObserver = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            titleH2.classList.add("animate__fadeInRight");
+            titleP.classList.add("animate__fadeInRight");
+            titleHr.classList.add("animate__fadeInRight");
+            observer.unobserve(entry.target);
+        }
+    });
+});
+
+titleObserver.observe(section);
+
+const firstStory = document.querySelector(".story-card-one");
+const fStoryRight = document.querySelector(".st-card-text-one")
+const fStoryLeft = document.querySelector(".st-card-img-one")
+
+
+const firstStoryObserver = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            fStoryRight.classList.add("animate__fadeInRight");
+            fStoryLeft.classList.add("animate__fadeInLeft");
+            observer.unobserve(entry.target);
+        }
+    });
+});
+
+firstStoryObserver.observe(firstStory);
+
+firstStory.addEventListener("mouseenter", (e) => {
+    fStoryLeft.classList.add("forScale");
+});
+
+
+firstStory.addEventListener("mouseleave", (e) => {
+    fStoryLeft.classList.remove("forScale");
+});
+
+
+const secondStory = document.querySelector(".story-card-two");
+const sStoryRight = document.querySelector(".st-card-img-two")
+const sStoryLeft = document.querySelector(".st-card-text-two")
+
+
+
+const secondStoryObserver = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            sStoryRight.classList.add("animate__fadeInRight");
+            sStoryLeft.classList.add("animate__fadeInLeft");
+            observer.unobserve(entry.target);
+        }
+    });
+});
+
+secondStoryObserver.observe(secondStory);
+
+secondStory.addEventListener("mouseenter", (e) => {
+
+    sStoryRight.classList.add("forScale");
+});
+
+
+secondStory.addEventListener("mouseleave", (e) => {
+
+    sStoryRight.classList.remove("forScale");
+});
+
+
+const thirdStory = document.querySelector(".story-card-three");
+const thStoryRight = document.querySelector(".st-card-text-three")
+
+const thStoryLeft = document.querySelector(".st-card-img-three")
+
+
+
+const thridStoryObserver = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            thStoryRight.classList.add("animate__fadeInRight");
+            thStoryLeft.classList.add("animate__fadeInLeft");
+            observer.unobserve(entry.target);
+        }
+    });
+});
+
+thridStoryObserver.observe(thirdStory);
+
+
+thirdStory.addEventListener("mouseenter", (e) => {
+
+    thStoryLeft.classList.add("forScale");
+});
+
+
+thirdStory.addEventListener("mouseleave", (e) => {
+
+    thStoryLeft.classList.remove("forScale");
+});
+
+
+
+
+// Video modal
+
+
+const VideoModal = document.getElementById("videoModal");
+
+
+const btn = document.getElementById("forVideoModal");
+
+
+const span = document.querySelector(".VideoModalClose");
+
+
+btn.onclick = function () {
+    VideoModal.style.display = "block";
+}
+
+
+span.onclick = function () {
+    VideoModal.style.display = "none";
+}
+
+
+window.onclick = function (event) {
+    if (event.target == VideoModal) {
+        VideoModal.style.display = "none";
+    }
+}
+
+
+
+
+
+
+
+
+
